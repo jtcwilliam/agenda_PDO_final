403b87f57d5e1e6d2a75d67cf6f29c5d7457dea0
William Ferreira <jtcwilliam@gmail.com>
2025-06-05 18:06:12 -0300

    finalizada classe

--- pwd/classes/enviarEmail.php
+++ pwd/classes/enviarEmail.php
@@ -7,6 +7,6 @@ $objEnvio = new Envio();
 
 $objEnvio->setDestinatario('viniciuscostapmg@gmail.com');
 $objEnvio->setAssunto('Ve ai se recebe');
-$objEnvio->setConteudo('Estamos   enviando a senha');
+$objEnvio->setConteudo('vinicius boa noite, compra tadala, vai que precisa');
 
 $objEnvio->envioEmail();
